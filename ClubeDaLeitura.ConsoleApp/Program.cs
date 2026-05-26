7a98de803df1ec4dffd62de22ef77074f3cb0046
guilhermepneto <gui.pereiraneto@hotmail.com>
2026-05-26 19:35:37 -0300

    Add informações iniciais

--- ClubeDaLeitura.ConsoleApp/Program.cs
+++ ClubeDaLeitura.ConsoleApp/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using ClubeDaLeitura.ConsoleApp.Compartilhado;
+
+TelaPrincipal telaPrincipal = new TelaPrincipal();
+
+while (true)
+{
+    string? opcaoMenuPrincipal = telaPrincipal.ObterOpcaoMenuPrincipal();
+
+    if (opcaoMenuPrincipal == "S")
+    {
+        break;
+    }
+
+    if (opcaoMenuPrincipal == "1")
+    {
+
+    }
+
+    else if (opcaoMenuPrincipal == "2")
+    {
+
+    }
+
+    else if (opcaoMenuPrincipal == "3")
+    {
+
+    }
+
+    else if (opcaoMenuPrincipal == "4")
+    {
+
+    }
+
+}
--- ClubeDaLeitura.ConsoleApp/obj/Debug/net10.0/ClubeDaLeitura.ConsoleApp.AssemblyInfo.cs
+++ ClubeDaLeitura.ConsoleApp/obj/Debug/net10.0/ClubeDaLeitura.ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClubeDaLeitura.ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4d8a8b8aa9fda9a29be538be74fbda2b0ed8af8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClubeDaLeitura.ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClubeDaLeitura.ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
